a13d292294bcbbd9da161e67a583a4e6410eabbf
johnathan nguyen <nonosnguyen@gmail.com>
2022-07-26 18:48:56 +0200

    3. add schema pydantic

--- app/main.py
+++ app/main.py
@@ -1,27 +1,10 @@
+from fastapi import FastAPI, APIRouter, Query
+
+from app.recipe_data import RECIPES
+from app.schemas import RecipeSearchResults, Recipe, RecipeCreate
+
 from typing import Optional
 
-from fastapi import FastAPI, APIRouter
-
-RECIPES = [
-    {
-        "id": 1,
-        "label": "Chicken Vesuvio",
-        "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
-    },
-    {
-        "id": 2,
-        "label": "Chicken Paprikash",
-        "source": "No Recipes",
-        "url": "http://norecipes.com/recipe/chicken-paprikash/",
-    },
-    {
-        "id": 3,
-        "label": "Cauliflower and Tofu Curry Recipe",
-        "source": "Serious Eats",
-        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
-    },
-]
 
 # https://christophergs.com/tutorials/ultimate-fastapi-tutorial-pt-1-hello-world/
 # 1 We instantiate a FastAPI app object, which is a
@@ -45,12 +28,11 @@ def root() -> dict:
 # https://fastapi.tiangolo.com/tutorial/path-params/
 # This is because FastAPI is coercing the input parameter type
 # based on the function argument type hints. This is a handy way of preventing input errors.
-@api_router.get("/recipe/{recipe_id}", status_code=200)
+@api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
 def fetch_recipe(*, recipe_id: int) -> dict:
     """
     Fetch a single recipe by ID
     """
-    print(type(recipe_id))  # ADDED
 
     result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
     if result:
@@ -59,9 +41,11 @@ def fetch_recipe(*, recipe_id: int) -> dict:
 
 # New addition, query parameter
 # https://fastapi.tiangolo.com/tutorial/query-params/
-@api_router.get("/search/", status_code=200)
+@api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
 def search_recipes(
-    keyword: Optional[str] = None, max_results: Optional[int] = 10
+    *,
+    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),  # 2
+    max_results: Optional[int] = 10
 ) -> dict:
     """
     Search for recipes based on label keyword
@@ -73,6 +57,26 @@ def search_recipes(
 
     results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
     return {"results": list(results)[:max_results]}
+
+
+# New addition, using Pydantic model `RecipeCreate` to define
+# the POST request body
+# 1
+@api_router.post("/recipe/", status_code=201, response_model=Recipe)
+def create_recipe(*, recipe_in: RecipeCreate) -> dict:  # 2
+    """
+    Create a new recipe (in memory only)
+    """
+    new_entry_id = len(RECIPES) + 1
+    recipe_entry = Recipe(
+        id=new_entry_id,
+        label=recipe_in.label,
+        source=recipe_in.source,
+        url=recipe_in.url,
+    )
+    RECIPES.append(recipe_entry.dict())  # 3
+
+    return recipe_entry
 
 
 # 4 to register the router
