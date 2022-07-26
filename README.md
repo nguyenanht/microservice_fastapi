da8b00b004d13eefedd42aed8c44abe5a0308173
johnathan nguyen <nonosnguyen@gmail.com>
2022-07-26 19:33:23 +0200

    6. Setting Up Database Tables with SQLAlchemy and Pydantic DB Schemas and CRUD Utilities

--- README.md
+++ README.md
@@ -9,3 +9,61 @@
 5. Open http://localhost:8001/
 
 To stop the server, press CTRL+C
+
+
+## 6 Database Setup with SQLAlchemy and Alembic
+https://christophergs.com/tutorials/ultimate-fastapi-tutorial-pt-7-sqlalchemy-database-setup/
+
+SQLAlchemy is one of the most widely used and highest quality Python third-party libraries. It gives application developers easy ways to work with relational databases in their Python code.
+
+SQLAlchemy is composed of two distinct components:
+
+Core - a fully featured SQL abstraction toolkit
+ORM (Object Relational Mapper) - which is optional
+
+In this tutorial, we will make use of both components, though you can adapt the approach not to use the ORM.
+
+```bash
+.
+├── alembic                    ----> NEW
+│  ├── env.py
+│  ├── README
+│  ├── script.py.mako
+│  └── versions
+│     └── 238090727082_added_user_and_recipe_tables.py
+├── alembic.ini                ----> NEW
+├── app
+│  ├── __init__.py
+│  ├── backend_pre_start.py    ----> NEW
+│  ├── crud                    ----> NEW
+│  │  ├── __init__.py
+│  │  ├── base.py
+│  │  ├── crud_recipe.py
+│  │  └── crud_user.py
+│  ├── db                      ----> NEW
+│  │  ├── __init__.py
+│  │  ├── base.py
+│  │  ├── base_class.py
+│  │  ├── init_db.py
+│  │  └── session.py
+│  ├── deps.py                 ----> NEW
+│  ├── initial_data.py
+│  ├── main.py
+│  ├── models                  ----> NEW
+│  │  ├── __init__.py
+│  │  ├── recipe.py
+│  │  └── user.py
+│  ├── recipe_data.py
+│  ├── schemas
+│  │  ├── __init__.py
+│  │  ├── recipe.py
+│  │  └── user.py
+│  └── templates
+│     └── index.html
+├── poetry.lock
+├── prestart.sh
+├── pyproject.toml
+├── README.md
+└── run.sh
+
+```
--- app/main.py
+++ app/main.py
@@ -1,111 +1,99 @@
-from fastapi import FastAPI, APIRouter, Query, HTTPException, Request
+from fastapi import FastAPI, APIRouter, Query, HTTPException, Request, Depends
 from fastapi.templating import Jinja2Templates
-
-from app.recipe_data import RECIPES
-from app.schemas import RecipeSearchResults, Recipe, RecipeCreate
 
 from typing import Optional, Any
 from pathlib import Path
+from sqlalchemy.orm import Session
 
+from app.schemas.recipe import RecipeSearchResults, Recipe, RecipeCreate
+from app import deps
+from app import crud
+
+
+# Project Directories
+ROOT = Path(__file__).resolve().parent.parent
 BASE_PATH = Path(__file__).resolve().parent
 TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
-# https://christophergs.com/tutorials/ultimate-fastapi-tutorial-pt-1-hello-world/
-# 1 We instantiate a FastAPI app object, which is a
-# Python class that provides all the functionality for your API.
+
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
-# 2 We instantiate an APIRouter which is how we can group our API endpoints (and specify versions and other config which we will look at later)
 api_router = APIRouter()
 
-# 3 By adding the @api_router.get("/", status_code=200)
-# decorator to the root function, we define a basic GET endpoint for our API.
+
+# Updated to serve a Jinja2 template
+# https://www.starlette.io/templates/
+# https://jinja.palletsprojects.com/en/3.0.x/templates/#synopsis
 @api_router.get("/", status_code=200)
-def root(request: Request) -> dict:
+def root(
+    request: Request,
+    db: Session = Depends(deps.get_db),
+) -> dict:
     """
     Root GET
     """
+    recipes = crud.recipe.get_multi(db=db, limit=10)
     return TEMPLATES.TemplateResponse(
-        "index.html", {"request": request, "recipes": RECIPES},
+        "index.html",
+        {"request": request, "recipes": recipes},
     )
 
 
-# New addition, path parameter
-# https://fastapi.tiangolo.com/tutorial/path-params/
-# This is because FastAPI is coercing the input parameter type
-# based on the function argument type hints. This is a handy way of preventing input errors.
 @api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
-def fetch_recipe(*, recipe_id: int) -> Any:
+def fetch_recipe(
+    *,
+    recipe_id: int,
+    db: Session = Depends(deps.get_db),
+) -> Any:
     """
     Fetch a single recipe by ID
     """
-
-    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    result = crud.recipe.get(db=db, id=recipe_id)
     if not result:
         # the exception is raised, not returned - you will get a validation
         # error otherwise.
-        # 2
         raise HTTPException(
             status_code=404, detail=f"Recipe with ID {recipe_id} not found"
         )
 
-    return result[0]
+    return result
 
 
-# New addition, query parameter
-# https://fastapi.tiangolo.com/tutorial/query-params/
 @api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
 def search_recipes(
     *,
-    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),  # 2
+    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
     max_results: Optional[int] = 10,
+    db: Session = Depends(deps.get_db),
 ) -> dict:
     """
     Search for recipes based on label keyword
     """
+    recipes = crud.recipe.get_multi(db=db, limit=max_results)
     if not keyword:
-        # we use Python list slicing to limit results
-        # based on the max_results query parameter
-        return {"results": RECIPES[:max_results]}
+        return {"results": recipes}
 
-    results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
+    results = filter(lambda recipe: keyword.lower() in recipe.label.lower(), recipes)
     return {"results": list(results)[:max_results]}
 
 
-# New addition, using Pydantic model `RecipeCreate` to define
-# the POST request body
-# 1
 @api_router.post("/recipe/", status_code=201, response_model=Recipe)
-def create_recipe(*, recipe_in: RecipeCreate) -> dict:  # 2
+def create_recipe(
+    *, recipe_in: RecipeCreate, db: Session = Depends(deps.get_db)
+) -> dict:
     """
-    Create a new recipe (in memory only)
+    Create a new recipe in the database.
     """
-    new_entry_id = len(RECIPES) + 1
-    recipe_entry = Recipe(
-        id=new_entry_id,
-        label=recipe_in.label,
-        source=recipe_in.source,
-        url=recipe_in.url,
-    )
-    RECIPES.append(recipe_entry.dict())  # 3
+    recipe = crud.recipe.create(db=db, obj_in=recipe_in)
 
-    return recipe_entry
+    return recipe
 
 
-# 4 to register the router
 app.include_router(api_router)
 
 
-# 5 The __name__ == "__main__" conditional applies when a module is called directly,
-# i.e. if we run python app/main.py. In this scenario, we need to import uvicorn since FastAPI
-# depends on this web server (which we’ll talk more about later)
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
-    # by default, the framework is based on OpenAPI,
-    # there are multiple options, 2 included by default
-    # https://fastapi.tiangolo.com/features/:
-    # - http://127.0.0.1:8000/redoc
-    # - http://127.0.0.1:8000/docs
