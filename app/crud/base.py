577662cf94ea1e6d3778a067a4c617183f226019
johnathan nguyen <nonosnguyen@gmail.com>
2022-07-27 11:39:57 +0200

    8. async

--- app/crud/base.py
+++ app/crud/base.py
@@ -25,9 +25,11 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         return db.query(self.model).filter(self.model.id == id).first()
 
     def get_multi(
-        self, db: Session, *, skip: int = 0, limit: int = 100
+        self, db: Session, *, skip: int = 0, limit: int = 5000
     ) -> List[ModelType]:
-        return db.query(self.model).offset(skip).limit(limit).all()
+        return (
+            db.query(self.model).order_by(self.model.id).offset(skip).limit(limit).all()
+        )
 
     def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
         obj_in_data = jsonable_encoder(obj_in)
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import time
 from pathlib import Path
 
 from fastapi import FastAPI, APIRouter, Request, Depends
@@ -13,7 +14,7 @@ BASE_PATH = Path(__file__).resolve().parent
 TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
 root_router = APIRouter()
-app = FastAPI(title="Recipe API")
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 
 @root_router.get("/", status_code=200)
@@ -29,6 +30,15 @@ def root(
         "index.html",
         {"request": request, "recipes": recipes},
     )
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
